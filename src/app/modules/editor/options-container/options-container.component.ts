161da85088e6999a7cf2950e1c5a0b5ed5877c31
Barzu Gabriel <barzu.gabriel@yahoo.com>
2020-10-05 08:49:56 +0300

    Add slider

--- src/app/modules/editor/options-container/options-container.component.ts
+++ src/app/modules/editor/options-container/options-container.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-options-container',
   templateUrl: './options-container.component.html',
   styleUrls: ['./options-container.component.scss']
 })
-export class OptionsContainerComponent implements OnInit {
+export class OptionsContainerComponent {
 
-  constructor() { }
+  formatLabel(value: number) {
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
 
-  ngOnInit(): void {
+    return value;
   }
 
 }
